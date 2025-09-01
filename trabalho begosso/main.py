0251a4801eca3864d7fdf425cbc836f901653c57
gohenj <gusgustaca@gmail.com>
2025-09-01 20:39:47 -0300

    n sei pq n foi

--- trabalho begosso/main.py
+++ trabalho begosso/main.py
@@ -12,3 +12,27 @@ class Aluno:
         self.data_nascimento = data_nascimento
         self.peso = peso
         self.altura = altura
+class Professores:
+    def __init__(self, codigo, nome, endereco, telefone, codigo_cidade):
+        self.codigo = codigo
+        self.nome = nome
+        self.endereco = endereco
+        self.telefone = telefone
+        self.codigo_cidade = codigo_cidade
+
+class Modalidade:
+    def __init__(self, codigo, descricao, codigo_professor, valor_aula, limite_alunos, total_alunos):
+        self.codigo = codigo
+        self.descricao = descricao
+        self.codigo_professor = codigo_professor
+        self.valor_aula = valor_aula
+        self.limite_alunos = limite_alunos
+        self.total_alunos = total_alunos
+
+class Matricula:
+    def __init__(self, codigo, codigo_aluno, codigo_modalidade, data_matricula, quantidade_aulas):
+        self.codigo = codigo
+        self.codigo_aluno = codigo_aluno
+        self.codigo_modalidade = codigo_modalidade
+        self.qtdde_aulas = quantidade_aulas
+        
